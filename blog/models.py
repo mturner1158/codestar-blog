4f32076a7bc4fc87e91b5c38251a2b3365fc87ca
mturner1158 <mpt4583@gmail.com>
2026-08-29 16:06:15 +0100

    made comment model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,12 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="blog_comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
