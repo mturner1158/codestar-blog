2a7e3917a5590f55e2dfe1ae8273bf6c675acda5
mturner1158 <mpt4583@gmail.com>
2026-08-30 11:11:07 +0100

    added package

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['mt-blog-project-4dc2a9520f5d.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
