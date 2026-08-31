57b9d692fe6d6d61da3ae7e3750d715492f77382
mturner1158 <mpt4583@gmail.com>
2026-08-31 18:48:33 +0100

    updating for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mt-blog-project-4dc2a9520f5d.herokuapp.com', 'localhost', '127.0.0.1']
 
