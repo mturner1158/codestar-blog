b81b4ca81e42826837f7b28980cc4bc096c45a98
mturner1158 <mpt4583@gmail.com>
2026-09-01 20:03:53 +0100

    fixed default value in blog comments model

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField()
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
