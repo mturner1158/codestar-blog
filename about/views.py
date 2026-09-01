1acdace242980035be520cae5ed49282369ff2c4
mturner1158 <mpt4583@gmail.com>
2026-09-01 20:36:15 +0100

    added new form to about app

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 
 # Create your views here.
@@ -19,9 +20,11 @@ def about_detail(request):
     """
 
     queryset = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": queryset},
+        {"about": queryset,
+         "collaborate_form": collaborate_form},
     )
